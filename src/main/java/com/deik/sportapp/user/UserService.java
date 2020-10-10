dc3f0b293153816b05532b7d8bf3e12c51e4cbe8
whoisZORZ <orszaghlev@gmail.com>
2020-10-10 16:43:30 +0200

    Updated UserService with methods provided by CrudRepository

--- src/main/java/com/deik/sportapp/user/UserService.java
+++ src/main/java/com/deik/sportapp/user/UserService.java
@@ -1,43 +1,37 @@
 package com.deik.sportapp.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private List<User> users = new ArrayList<>(Arrays.asList(
-       new User("1", "Geza", "Pelda", "gezapelda@gmail.com"),
-       new User("2", "Aladar", "Minta", "aladarminta@gmail.com")
-    ));
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
         return users;
     }
 
     public User getUser(String id) {
-        return users.stream().filter(u -> u.getId().equals(id)).findFirst().get();
+        return userRepository.findById(id).orElse(null);
     }
 
     public void addUser(User user) {
-        users.add(user);
+        userRepository.save(user);
     }
 
     public void updateUser(String id, User user) {
-        for (int i = 0; i < users.size(); i++) {
-            User u = users.get(i);
-            if (u.getId().equals(id)) {
-                users.set(i, user);
-                return;
-            }
-        }
+        userRepository.save(user);
     }
 
     public void deleteUser(String id) {
-        users.removeIf(u -> u.getId().equals(id));
+        userRepository.deleteById(id);
     }
 
 }
