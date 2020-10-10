a42e75bec2d529c5046f32a8e7ea2020acd1408d
whoisZORZ <orszaghlev@gmail.com>
2020-10-10 15:59:36 +0200

    Updated UserService with addUser method in order to add a new user

--- src/main/java/com/deik/sportapp/user/UserController.java
+++ src/main/java/com/deik/sportapp/user/UserController.java
@@ -2,6 +2,7 @@ package com.deik.sportapp.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,11 @@ public class UserController {
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable String id) {
         return userService.getUser(id);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
     }
 
 }
--- src/main/java/com/deik/sportapp/user/UserService.java
+++ src/main/java/com/deik/sportapp/user/UserService.java
@@ -2,16 +2,17 @@ package com.deik.sportapp.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private List<User> users = Arrays.asList(
+    private List<User> users = new ArrayList<>(Arrays.asList(
        new User("1", "Geza", "Pelda", "gezapelda@gmail.com"),
        new User("2", "Aladar", "Minta", "aladarminta@gmail.com")
-    );
+    ));
 
     public List<User> getAllUsers() {
         return users;
@@ -19,6 +20,10 @@ public class UserService {
 
     public User getUser(String id) {
         return users.stream().filter(u -> u.getId().equals(id)).findFirst().get();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
     }
 
 }
