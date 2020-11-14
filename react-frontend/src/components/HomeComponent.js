35208be94479c6979e353ce136081649fbf78f1c
unknown <orszaghlev@gmail.com>
2020-11-14 19:54:40 +0100

    Updated home component with authorization

--- react-frontend/src/components/HomeComponent.js
+++ react-frontend/src/components/HomeComponent.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import AuthService from "../services/AuthService.js";
 import UserService from "../services/UserService.js";
 
 export default class Home extends Component {
@@ -6,11 +7,19 @@ export default class Home extends Component {
         super(props);
 
         this.state = {
+            redirect: null,
+            userReady: false,
+            currentUser: {username: ""},
             content: ""
         };
     }
 
     componentDidMount() {
+        const currentUser = AuthService.getCurrentUser();
+        if (currentUser) {
+            this.setState({currentUser: currentUser, userReady: true})
+        }
+
         UserService.getPublicContent().then(
             response => {
                 this.setState({
@@ -28,12 +37,21 @@ export default class Home extends Component {
         );
     }
 
+    getUserLoginInfo() {
+        if (!this.state.userReady) {
+           return <h3><a href="http://localhost:8081/register">Sign up</a> or <a href="http://localhost:8081/login">log in</a> to access content!</h3>
+        } else {
+           return <h3>Welcome {this.state.currentUser.username}!</h3>
+        }
+    }
+
     render() {
         return (
             <div className="container">
                 <header className="jumbotron">
                     <h3>Sport app project for DE-IK Software Development Methodologies course.</h3>
-                    <h3><a href="http://localhost:8081/register">Sign up</a> or <a href="http://localhost:8081/login">log in</a> to access content!</h3>
+                    <br></br>
+                    {this.getUserLoginInfo()}
                 </header>
             </div>
         );
