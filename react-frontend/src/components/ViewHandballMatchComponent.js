180ac852094e8c9f853b00ec5b6436d617697141
Marcell Szegedi <marcszeg27@gmail.com>
2020-11-16 16:08:46 +0100

    Finished the UI of handball matches

--- react-frontend/src/components/ViewHandballMatchComponent.js
+++ react-frontend/src/components/ViewHandballMatchComponent.js
@@ -60,69 +60,69 @@ class ViewHandballMatchComponent extends Component {
 
                         <table className="text-center align-middle table table-striped" style={{marginLeft: "0px", marginTop:"0px", marginBottom:"10px", fontSize:"16px", width:"483px"}}>
                             <tr>
-                                <td style={{width:"161px"}}></td>
+                                <td style={{width:"161px"}}>{this.state.handballStats.hShootingEfficiency}</td>
                                 <td style={{width:"161px"}}>Shooting efficiency</td>
-                                <td style={{width:"161px"}}></td>
+                                <td style={{width:"161px"}}>{this.state.handballStats.aShootingEfficiency}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hWingGoals}</td>
+                                <td>Wing goals</td>
+                                <td>{this.state.handballStats.aWingGoals}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hFastbreakGoals}</td>
+                                <td>Fastbreak goals</td>
+                                <td>{this.state.handballStats.aFastbreakGoals}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hSevenMeters}</td>
+                                <td>7 meters</td>
+                                <td>{this.state.handballStats.aSevenMeters}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hSaves}</td>
+                                <td>Saves</td>
+                                <td>{this.state.handballStats.aSaves}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hTwoMinPenalty}</td>
+                                <td>2 minute penalties</td>
+                                <td>{this.state.handballStats.aTwoMinPenalty}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hYellowCards}</td>
+                                <td>Yellow cards</td>
+                                <td>{this.state.handballStats.aYellowCards}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hGoalStreak}</td>
+                                <td>Goal streak</td>
+                                <td>{this.state.handballStats.aGoalStreak}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hGoalsInPowerplay}</td>
+                                <td>Power play goals</td>
+                                <td>{this.state.handballStats.aGoalsInPowerplay}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hShorthandedGoals}</td>
+                                <td>Shorthanded goals</td>
+                                <td>{this.state.handballStats.aShorthandedGoals}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hSteals}</td>
+                                <td>Steals</td>
+                                <td>{this.state.handballStats.aSteals}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hTechnicalFaults}</td>
+                                <td>Technical faults</td>
+                                <td>{this.state.handballStats.aTechnicalFaults}</td>
                             </tr>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.handballStats.hTimeouts}</td>
+                                <td>Timeouts</td>
+                                <td>{this.state.handballStats.aTimeouts}</td>
                             </tr>
                         </table>
 
